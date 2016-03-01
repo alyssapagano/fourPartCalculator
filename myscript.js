bafbac5dcf6a6a7565a3d0ee2a99336cde4f8965
alyssapagano <alyssa.pagano@journalism.cuny.edu>
2016-03-01 18:26:29 -0500

    updated calc

--- myscript.js
+++ myscript.js
@@ -1,32 +1,43 @@
 //add
-
 //the goal of the function is to a string that says
   //num1 + num2 = result
 
   //make sure your numbers are numbers, no string
 function addNums(num1, num2){ 
-
-  var num1 = document.getElementById('first').value
-  var num2 = document.getElementById('second').value
   var addResult = parseFloat(num1) + parseFloat(num2),
-      
       stringOne = num1 + '+' + num2 + '=' + addResult;
       
-      onclick=addNums(){
-document.getElementByID('result').innerHTML=stringOne;
-}
+    return stringOne;
 }
 
 function subtractNums(num1, num2){
-  var subtractResult = parseFloat(num1) - parseFloat(num2);
-stringTwo = num1 + "-" + num2 + '=' + subtractResult;
+  var subtractResult = parseFloat(num1) - parseFloat(num2),
+stringTwo = num1 + '-' + num2 + '=' + subtractResult;
+
+  return stringTwo;
+}
+function multiplyNums(num1, num2){
+  var multResult = parseFloat(num1) + parseFloat(num2),
+  stringThree = num1 + '*' + num2 + '=' + multResult;
+
+  return stringThree;
+}
+function divideNums(num1, num2){
+  var divideResult = parseFloat(num1) / parseFloat(num2),
+  stringFour = num1 + "/" + num2 + '=' + divideResult;
+
+  return stringFour;
 }
 // substract
 
 // multiply
 
 // divide
-
+function populatehtml(arithmetic){
+var input1 = document.getElementById('first').value;
+var input2 = document.getElementById('second').value;
+var output = arithmetic(input1, input2);
+document.getElementById('result').innerText += output;
 // this function will grab the inputs, populate the result div
 // and run an operation
 
@@ -35,20 +46,25 @@ stringTwo = num1 + "-" + num2 + '=' + subtractResult;
 
   // if input1 or input2 is an empty string, output.innerText will be
   // set to a message that tells user they need to put in the numbers
-
+if (input1 == "" || input2 == ""){
+  document.getElementById('result').innerText="You need to put in some numbers, yo."
+} 
+}
   // remember the parameter operation is a function,
   // it needs its own arguments
 
 
 // grab a button element
 // to each button, attach a click event to which the appropriate operation is assigned
-document.getElementById('add').onclick = function(){populatehtml(addNums)};
+document.getElementById('add').onclick = function(){populatehtml(addNums);
+}
 
-document.getElementById('subtract').onclick = function(){
-    populatehtml(subtract) };
+document.getElementById('subtract').onclick = function(){populatehtml(subtractNums);
+}
 
 document.getElementById('multiply').onclick = function(){
-    populatehtml(multiply) };
+    populatehtml(multiplyNums) ;
+  }
 
 document.getElementById('divide').onclick = function(){
-    populatehtml(divide) };
+    populatehtml(divideNums) };
